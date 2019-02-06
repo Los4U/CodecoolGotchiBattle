47493517a0345367894f9396168f7f4a3a4d28ba
Piotr <gonska9933@gmail.com>
2019-02-06 17:23:49 +0100

    Init window in main class

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,5 +1,6 @@
 package com.codecool;
 
+import com.codecool.model.Game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,15 +9,14 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader;
-        fxmlLoader = new FXMLLoader( getClass().getResource("/fxml/register.fxml") );
+        FXMLLoader fxmlLoader =  new FXMLLoader( getClass().getResource("/fxml/register.fxml") );
+        Game game = Game.getInstance();
 
         Parent content = fxmlLoader.load();
         Scene mainScene = new Scene(content);
