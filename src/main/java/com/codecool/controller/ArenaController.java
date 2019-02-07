8802080d8028899d13d104a9ac511b0764a0f8e5
Piotr <gonska9933@gmail.com>
2019-02-07 18:36:15 +0100

    Little refactor using comments

--- src/main/java/com/codecool/controller/ArenaController.java
+++ src/main/java/com/codecool/controller/ArenaController.java
@@ -1,21 +1,52 @@
 package com.codecool.controller;
 
-import com.codecool.model.Battle;
+import com.codecool.model.Config;
 import com.codecool.model.Gotchi;
+import com.codecool.model.Type;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ArenaController implements Initializable {
+
+    // Player 1 state elements
     @FXML
     private Label player1NameTF;
 
     @FXML
+    private ImageView player1Image;
+
+    @FXML
+    private TextField player1HP;
+
+    @FXML
+    private TextField player1SP;
+
+
+    // Player 2 state elements
+    @FXML
     private Label player2NameTF;
+
+    @FXML
+    private ImageView player2Image;
+
+    @FXML
+    private TextField player2HP;
+
+    @FXML
+    private TextField player2SP;
+
+
+    //Other elements
+    @FXML
+    private TextField messageTF;
 
     @FXML
     private Button primaryAttackButton;
