4514d7325f9a0894f0fd470e4c45e1e53487579f
Kamil Woś <inz.kamil.wos@gmail.com>
2019-02-07 23:53:32 +0100

    Create carryAttack(String playerOneAcction) method which according player attack calculate the battle, and return new HP, STP, Status1, Status2

--- src/main/java/com/codecool/model/Battle.java
+++ src/main/java/com/codecool/model/Battle.java
@@ -25,7 +25,41 @@ public class Battle {
         initPlayer2("Computer's Gotchi", Type.ROCK, 50, 50, 50);
     }
 
+    public ArrayList<String> carryAttack(String playerOneAcction){
+        ArrayList<String> info = new ArrayList<>(5);
 
+        for (int i = 0; i < 5; i++) {
+            info.add("test");
+        }
+
+
+
+        if(playerOneAcction.equals("Primary")){
+            info.set(Config.PLAYER_1_STATUS, "Player: " + player1.getName() + " did PRIMARY ATTACK");
+            gotchi1stamina -= 20;
+            info.set(Config.PLAYER_1_STP, String.valueOf(gotchi1stamina));
+        }else if(playerOneAcction.equals("Secondary")){
+            info.set(Config.PLAYER_1_STATUS, "Player: " + player1.getName() + " did SECONDARY ATTACK");
+            gotchi1stamina -= 20;
+            info.set(Config.PLAYER_1_STP, String.valueOf(gotchi1stamina));
+
+        }else if(playerOneAcction.equals("Defense")){
+            info.set(Config.PLAYER_1_STATUS, "Player: " + player1.getName() + " did DEFENSE");
+            gotchi1stamina -= 10;
+            info.set(Config.PLAYER_1_STP, String.valueOf(gotchi1stamina));
+
+        }else if (playerOneAcction.equals("Evade")){
+            info.set(Config.PLAYER_1_STATUS, "Player: " + player1.getName() + " did EVADE");
+            gotchi1stamina -= 5;
+            info.set(Config.PLAYER_1_STP, String.valueOf(gotchi1stamina));
+        }
+
+        for(String elem: info){
+            System.out.println(elem);
+        }
+
+        return info;
+    }
 
 
 
