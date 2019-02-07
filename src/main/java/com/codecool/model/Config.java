2d462cb4df44b336a462650c48a24489dcb991e7
Kamil Woś <inz.kamil.wos@gmail.com>
2019-02-07 23:48:46 +0100

    Add statuses , convert magic numbers into config static final , PLAYER_1_STATUS, PLAYER_2_STATUS, HP etc.

--- src/main/java/com/codecool/model/Config.java
+++ src/main/java/com/codecool/model/Config.java
@@ -9,4 +9,18 @@ public class Config {
     public static final String MENU_SCREEN_PATH = "/fxml/menuScreen.fxml";
     public static final String MAIN_SCREEN_PATH = "/fxml/mainScreen.fxml";
     public static final String IMAGES_PATH = "/images/";
+
+
+    public static final int PLAYER_1_STATUS = 0;
+    public static final int PLAYER_2_STATUS = 1;
+
+    public static final int PLAYER_1_HP  = 2;
+    public static final int PLAYER_1_STP = 3;
+
+    public static final int PLAYER_2_HP = 4;
+
+
+
+
+
 }
