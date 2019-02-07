c06982b78d33b77dfa96a02e49ef4e692f837740
Kamil Woś <inz.kamil.wos@gmail.com>
2019-02-07 20:32:51 +0100

    add gotchi health and stamina

--- src/main/java/com/codecool/model/Battle.java
+++ src/main/java/com/codecool/model/Battle.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Battle {
+
     private Gotchi player1;
     private Gotchi player2;
+    private int gotchi1health = Config.MAX_HEALTH ;
+    private int gotchi2health = Config.MAX_HEALTH ;
+    private int gotchi1stamina = Config.MAX_STAMINA;
+    private int gotchi2stamina = Config.MAX_STAMINA;;
+
 
     public void initPlayer1(String name, Type type, int attack, int speed, int defense){
         player1 = new Gotchi(name, type, attack, speed, defense);
@@ -18,6 +24,10 @@ public class Battle {
     public void createGotchiByPC(){
         initPlayer2("Computer's Gotchi", Type.ROCK, 50, 50, 50);
     }
+
+
+
+
 
     public Gotchi getPlayer1() {
         return player1;
